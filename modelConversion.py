ed59d6ddb8f7a7ee42abbf9e6cbe0a696edeb5dc
arshiaesll <arshia.esl83@gmail.com>
2025-02-22 21:21:04 -0500

    Adding the coreML model

--- modelConversion.py
+++ modelConversion.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 from OriginalLeNet import OGLeNet5 as LeNet
 from PIL import Image
+import coremltools as ct
+import os
+
 def load_and_run_model(model_path, example_input):
     try:
         # Load the model
@@ -28,15 +31,27 @@ def load_and_run_model(model_path, example_input):
         print(f"Error loading or running model: {str(e)}")
         return None
 
+def convert_to_coreml(model_path):
+    model = LeNet()
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+    example_input = torch.randn(1, 1, 32, 32)
+    traced_model = torch.jit.trace(model, example_input)
+    coreML_model = ct.convert(traced_model,
+                              inputs=[ct.TensorType(shape = example_input.shape)],
+                              compute_units = ct.ComputeUnit.ALL)
+    coreML_model.save("LeNet.mlpackage")
+
+
 if __name__ == "__main__":
     # Example usage
     model_path = "./model.pt"
-    image = Image.open("test_image.png")
+    # image = Image.open("test_image.png")
     # Create example input data - LeNet expects 32x32 images with 1 channel
     example_data = torch.randn(1, 1, 32, 32)  # (batch_size, channels, height, width)
     
     # Load and run the model
     output = load_and_run_model(model_path, example_data)
-    
+    convert_to_coreml(os.path.join( os.path.dirname(model_path), "model.pt" ))
     if output is not None:
         print("Model output:", output)
